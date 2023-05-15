95dc6fe621f1ec1ad6888673773ff26cbab2d3f2
iamstefano <stefanoar3@gmail.com>
2023-05-15 17:53:30 +0200

    15-05-2023 Es.1 e Avanzato, filters e responsive Inizio hamburger menu

---
+++ 15-05-2023/utils/fn.js
@@ -0,0 +1,42 @@
+export const cE = (el) => document.createElement(el);
+
+export const qS = (el) => document.querySelector(el);
+
+export const formatDescriptionText = (str) =>
+  str.split(" ").splice(0, 5).join(" ") + " ...";
+
+export const createProduct = (data) => {
+  const wrapperEl = cE("div");
+  const textWrapperEl = cE("div");
+  const imageEl = cE("img");
+  const titleEl = cE("h3");
+  const descriptionEl = cE("p");
+  const ratingEl = cE("p");
+  const priceEl = cE("h4");
+  const buttonEl = cE("button");
+  const favEl = cE("button");
+
+  wrapperEl.className = "productCard";
+  textWrapperEl.className = "productCard__text";
+  imageEl.src = data.thumbnail;
+  imageEl.alt = data.title;
+  titleEl.textContent = data.title;
+  descriptionEl.textContent = formatDescriptionText(data.description);
+  ratingEl.textContent = data.rating;
+  priceEl.textContent = data.price + " $";
+  buttonEl.textContent = "Add to Cart";
+  favEl.textContent = "❤️";
+
+  textWrapperEl.append(
+    titleEl,
+    descriptionEl,
+    ratingEl,
+    priceEl,
+    buttonEl,
+    favEl
+  );
+
+  wrapperEl.append(imageEl, textWrapperEl);
+
+  return wrapperEl;
+};
