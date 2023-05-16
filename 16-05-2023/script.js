622118c1ab23dcdb972ed3ff3dd3f4af4824bf56
iamstefano <stefanoar3@gmail.com>
2023-05-16 17:18:57 +0200

    16-05-2023 Esercizio 1 commit

---
+++ 16-05-2023/script.js
@@ -0,0 +1,48 @@
+import { cE, qS, createProduct, createProductModal, qSA } from "./utils/fn.js";
+
+/* const productMock = {
+  id: 1,
+  title: "iPhone 9",
+  description: "An apple mobile which is nothing like apple",
+  price: 549,
+  discountPercentage: 12.96,
+  rating: 4.69,
+  stock: 94,
+  brand: "Apple",
+  category: "smartphones",
+  thumbnail: "https://i.dummyjson.com/data/products/1/thumbnail.jpg",
+  images: [
+    "https://i.dummyjson.com/data/products/1/1.jpg",
+    "https://i.dummyjson.com/data/products/1/2.jpg",
+    "https://i.dummyjson.com/data/products/1/3.jpg",
+    "https://i.dummyjson.com/data/products/1/4.jpg",
+    "https://i.dummyjson.com/data/products/1/thumbnail.jpg",
+  ],
+}; */
+
+const rootEl = qS("#root");
+const productList = cE("div");
+const productListTitle = cE("h2");
+
+productList.className = "productList";
+productListTitle.textContent = "Our products for you:";
+rootEl.append(productListTitle, productList);
+
+fetch("https://dummyjson.com/products")
+  .then((res) => res.json())
+  .then((data) => {
+    data.products.forEach((product) =>
+      productList.append(createProduct(product))
+    );
+  })
+  .then(() => {
+    const productCardEls = qSA(".productCard");
+
+    productCardEls.forEach((product) =>
+      product.addEventListener("click", () =>
+        fetch(`https://dummyjson.com/products/${product.id}`)
+          .then((res) => res.json())
+          .then((data) => rootEl.append(createProductModal(data, rootEl)))
+      )
+    );
+  });
