1ba5f2a668cee3e12b552a3a65c3bf9a5aad6e99
Iamstefanoe <stefanoar3@gmail.com>
2023-05-25 16:58:29 +0200

    25/05/2023 esercizio 1 simulazione Fetch try&catch

---
+++ 25-05-2023/script.js
@@ -0,0 +1,23 @@
+import { getToDoUsers } from "./fn/http.js";
+
+const qS = (el) => document.querySelector(el);
+const cE = (el) => document.createElement(el);
+
+let addToDoButton = document.getElementById("addToDo");
+let toDoContainer = document.getElementById("toDoContainer");
+let inputField = document.getElementById("inputField");
+
+/* addToDoButton.addEventListener("click", () => {
+  let paragraph = document.createElement("p");
+  paragraph.classList.add("paragraph-styling");
+  paragraph.innerText = inputField.value;
+  toDoContainer.appendChild(paragraph);
+  inputField.value = "";
+  paragraph.addEventListener("click", () => {
+    paragraph.style.textDecoration = "line-through";
+  });
+  paragraph.addEventListener("dblclick", () => {
+    toDoContainer.removeChild(paragraph);
+  });
+});
+ */
