9d7f33e933d2558337ff40114fdce01724373bfc
Iamstefanoe <stefanoar3@gmail.com>
2023-05-29 18:53:59 +0200

    29-05-2023 esercizio 1 content Home done

--- 29-05-2023/js/app.js
+++ 29-05-2023/js/app.js
@@ -32,7 +32,7 @@ const createEl = (
 
 const contentDiv = qS(".content");
 const homeWrapper = createEl("div", "home_wrapper", null, contentDiv);
-const homeEl = createEl("h2", null, "Home", homeWrapper);
-const forYouWrappEl = createEl("div", "wrapper_for_you", null, homeWrapper);
-const forYouP = createEl("p", "for_you", "for you", forYouWrappEl);
-const followingP = createEl("p", "follow", "following", forYouWrappEl);
+const homeEl = createEl("h2", "homeEl", "Home", homeWrapper);
+const forYouWrappEl = createEl("div", "forYou_wrapper", null, homeWrapper);
+const forYouP = createEl("p", "for_you", "For you", forYouWrappEl);
+const followingP = createEl("p", "follow", "Following", forYouWrappEl);
