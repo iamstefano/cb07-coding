0d5789bf312a22e094da18f3068d99871efea5ee
Iamstefanoe <stefanoar3@gmail.com>
2023-05-30 00:14:33 +0200

    29-05-2023 Esercizio1 parte centrale app.js & css

--- 29-05-2023/js/app.js
+++ 29-05-2023/js/app.js
@@ -31,8 +31,44 @@ const createEl = (
 };
 
 const contentDiv = qS(".content");
+/****************Parte centrale Home/4you/following*************************************/
 const homeWrapper = createEl("div", "home_wrapper", null, contentDiv);
 const homeEl = createEl("h2", "homeEl", "Home", homeWrapper);
 const forYouWrappEl = createEl("div", "forYou_wrapper", null, homeWrapper);
 const forYouP = createEl("p", "for_you", "For you", forYouWrappEl);
-const followingP = createEl("p", "follow", "Following", forYouWrappEl);
+const followingP = createEl("p", "following", "Following", forYouWrappEl);
+/****************Parte centrale Tweet*************************************/
+const tweetWrapperHome = createEl(
+  "div",
+  "tweet_wrapper_home",
+  null,
+  contentDiv
+);
+const userAvatarInput = createEl(
+  "img",
+  "user_avatar_home",
+  null,
+  tweetWrapperHome,
+  {
+    name: "src",
+    value: "./images/The-Midnight-Gospel-serie-tv-Netflix.jpg",
+  }
+);
+const tweetForm = createEl(
+  "form",
+  "tweet_wrapper_form",
+  null,
+  tweetWrapperHome
+);
+const tweetInputEl = createEl("input", "tweet_input", null, tweetForm, {
+  name: "placeholder",
+  value: "what is happening?!",
+});
+const tweetSubmit = createEl("input", "tweet_submit", "tweet", tweetForm, {
+  name: "type",
+  value: "submit",
+});
+
+/****************Parte centrale Cards*************************************/
+
+const wrapperCardEl = createEl("div", "wrapper_card", null, contentDiv);
